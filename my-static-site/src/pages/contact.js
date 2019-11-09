be85311f431126afbf25cdf39473200b112f4fb8
DanNguyen8189 <dannguyen8189@gmail.com>
2019-11-09 14:42:38 -0800

    team and contact page styling

--- my-static-site/src/pages/contact.js
+++ my-static-site/src/pages/contact.js
@@ -1,63 +1,84 @@
-import React, { Component } from "react"
-import { Head } from 'react-static'
-import Navbar from '../components/Navbar'
-import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
+import React, { Component } from "react";
+import { Head } from "react-static";
+import Navbar from "../components/Navbar";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-import '../css/icon.css'
-import '../css/contact.css'
+import "../css/icon.css";
+import "../css/contact.css";
 
-const formEndpoint = 'https://formspree.io/xdqknwlm';
+const formEndpoint = "https://formspree.io/xdqknwlm";
 
 class Contact extends Component {
-
   render() {
     return (
       <div>
         <Head>
           <title>Contact</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          ></link>
         </Head>
         <Navbar />
-        <Container>
-          <header className="text-center">
-            <span className="icon fa-envelope featured" />
-            <h2 id="contact-header">Get In Touch</h2>
-            <p>Use the form below to send us an email!</p>
-          </header>
-          <Form
-            className="contact-form"
-            action={ formEndpoint }
-            method="POST"
-          >
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control name="name" type="text" placeholder="Enter name" />
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control name="email" type="email" placeholder="Enter email" />
-              </Form.Group>
-            </Form.Row>
+        <article id="main" className="contact-background">
+          <Container>
+            <header className="text-center">
+              <span className="icon fa-envelope featured" />
+              <h2 id="contact-header">Get In Touch</h2>
+              <p>Use the form below to send us an email!</p>
+            </header>
 
-            <Form.Group controlId="formGridSubject">
-              <Form.Label>Subject</Form.Label>
-              <Form.Control name="subject" placeholder="Enter subject" />
-            </Form.Group>
+            <section className="wrapper style2 special container medium">
+              <Form
+                className="contact-form"
+                action={formEndpoint}
+                method="POST"
+              >
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      name="name"
+                      type="text"
+                      placeholder="Enter name"
+                    />
+                  </Form.Group>
 
-            <Form.Group controlId="formGridMessage">
-              <Form.Label>Message</Form.Label>
-              <Form.Control name="message" as="textarea" rows="3" placeholder="Enter Message"/>
-            </Form.Group>
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      name="email"
+                      type="email"
+                      placeholder="Enter email"
+                    />
+                  </Form.Group>
+                </Form.Row>
 
-            <Button variant="primary" type="submit">
-              Send
-            </Button>
-          </Form>
-        </Container>
+                <Form.Group controlId="formGridSubject">
+                  <Form.Label>Subject</Form.Label>
+                  <Form.Control name="subject" placeholder="Enter subject" />
+                </Form.Group>
+
+                <Form.Group controlId="formGridMessage">
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control
+                    name="message"
+                    as="textarea"
+                    rows="3"
+                    placeholder="Enter Message"
+                  />
+                </Form.Group>
+
+                <Button id="submitButton" variant="primary" type="submit">
+                  Send
+                </Button>
+              </Form>
+            </section>
+          </Container>
+        </article>
       </div>
     );
   }
--- my-static-site/src/pages/team.js
+++ my-static-site/src/pages/team.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import { Head } from 'react-static'
-import Navbar from '../components/Navbar'
-import Container from 'react-bootstrap/Container'
-import Image from 'react-bootstrap/Image'
+import { Head } from "react-static";
+import Navbar from "../components/Navbar";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 import pieChart from "../images/major-distribution.png";
 import teamImage from "../images/team.jpg";
 
-import '../css/icon.css'
-import '../css/team.css';
-
+import "../css/icon.css";
+import "../css/team.css";
 
 class Team extends Component {
   render() {
@@ -17,112 +16,201 @@ class Team extends Component {
       <div>
         <Head>
           <title>Team</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          ></link>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="//fonts.googleapis.com/css?family=Cantarell"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="//fonts.googleapis.com/css?family=Open+Sans"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="//fonts.googleapis.com/css?family=Jura"
+          />
         </Head>
         <Navbar />
-        <Image src={ teamImage } fluid alt="Team"/>
+        <Image src={teamImage} fluid alt="Team" />
         <Container>
-        <div id="page-wrapper">
-          <article id="main">
-            <section>
-              
-            </section>
-            <section className="wrapper style4 container">
-              <div className="content">
-                <section>
-                  <header>
-                    <h3>The Roster</h3>
-                  </header>
-                  <p>
-                    The UCSD Solar Car Team has its roots firmly planted by a
-                    spirit of excellence through diversity. With a variety of
-                    students from contrasting educational as well as cultural
-                    backgrounds, every member is equally responsible for the
-                    team's success. Supervising them are the team leads, who go
-                    the extra step to bring out the best in every one of them.
-                  </p>
-                  <p />
-                  <p />
-                </section>
-              </div>
-            </section>
-            <section className="wrapper style1 container special">
-              <img id="piechart" src={ pieChart } alt="Pie Chart" />
-            </section>
-            <section className="wrapper style1 container special mt-5">
-              <div className="row">
-                <div className="col-4 col-12-narrower">
-                  <section>
-                    <span className="icon featured fa-briefcase" />
-                    <header>
-                      <h3>Project Lead: Kieu Huynh</h3>
-                    </header>
-                    <p className="text-muted">Class of 2020 | Mechanical Engineering</p>
-                  </section>
-                </div>
-                <div className="col-4 col-12-narrower">
-                  <section>
-                    <span className="icon featured fa-cogs" />
-                    <header>
-                      <h3>Mechanical Lead: Roy Choi</h3>
-                    </header>
-                    <p className="text-muted">Class of 2019 | Mechanical Engineering</p>
-                  </section>
-                </div>
-                <div className="col-4 col-12-narrower">
-                  <section>
-                    <span className="icon featured fa-bolt" />
-                    <header>
-                      <h3>Electrical Lead: Bryan Thai</h3>
-                    </header>
-                    <p className="text-muted">Class of 2019 | Bioengineering</p>
-                  </section>
-                </div>
-              </div>
-            </section>
-
-            <section className="wrapper style1 container special mt-3">
-              <span className="icon featured fa-users" />
-            </section>
-
-            <section className="wrapper style1 container special mt-3">
-              <div className="row">
-                <div className="col-4 col-12-narrower">
+          <div id="page-wrapper">
+            <article id="main">
+              <section></section>
+              <section className="wrapper style4 container">
+                <div className="content">
                   <section>
                     <header>
-                      <h3>Operations Team</h3>
+                      <h1 id="first-text">Meet our team!</h1>
                     </header>
-                    <p>Camila Paik, Angela Yu</p>
-                  </section>
-                </div>
-                <div className="col-4 col-12-narrower">
-                  <section>
-                    <header>
-                      <h3>Mechanical Team</h3>
-                    </header>
-                    <p>
-                      Roy Choi, Janet He, Eugine Lin, Angela Yu, Canwin Tso,
-                      Sean Orellana, Jennie Coates, Arjun Ray, Arturo Medina,
-                      Jesse Sanchez, Dustin Tengdyantono, Billy Park, Jacob
-                      Watson, Sukhdeep Chera,
+                    <p id="introduction">
+                      The UCSD Solar Car Team has its roots firmly planted by a
+                      spirit of excellence through diversity. With a variety of
+                      students from contrasting educational as well as cultural
+                      backgrounds, every member is equally responsible for the
+                      team's success. Supervising them are the team leads, who
+                      go the extra step to bring out the best in every one of
+                      them.
                     </p>
+                    <p />
+                    <p />
                   </section>
                 </div>
-                <div className="col-4 col-12-narrower">
-                  <section>
-                    <header>
-                      <h3>Electrical Team</h3>
-                    </header>
-                    <p>
-                      Bryan Thai, Dan Nguyen, Vivian Lee, Sara Dorr, Madeline
-                      Tjoa, Jonathan Venegas, Daniel Renfrow
-                    </p>
-                  </section>
+              </section>
+              <section className="wrapper style1 container special">
+                <img id="piechart" src={pieChart} alt="Pie Chart" />
+              </section>
+              {/*<section className="wrapper style1 container special mt-3">
+                <span className="icon featured fa-users" />
+    </section>*/}
+
+              <section className="wrapper style1 container special mt-5">
+                <div className="row">
+                  <div className="col-md-4 col-12-narrower lead">
+                    <section>
+                      <header>
+                        <div
+                          id="project-lead-img"
+                          className="profile-image"
+                        ></div>
+                        <h4>Kieu Huynh</h4>
+                      </header>
+                      <p className="profile-header">
+                        Project Lead | Class of 2020 | Mechanical Engineering
+                      </p>
+                      <p className="profile-description">
+                        “My fondest memory of the team would probably be our
+                        last end of the year bonfire of which I teamed up with a
+                        few other deviant members to spook the rest of the team.
+                        It definitely tested my abilities to maintain a poker
+                        face.”{" "}
+                      </p>
+                    </section>
+                  </div>
+                  <div className="col-md-4 col-12-narrower lead">
+                    <section>
+                      <header>
+                        <div
+                          id="mechanical-lead-img"
+                          className="profile-image"
+                        ></div>
+                        <h4>Roy Choi</h4>
+                      </header>
+                      <p className="profile-header">
+                        Mechanical Lead | Class of 2020 | Mechanical Engineering
+                      </p>
+                      <p className="profile-description">
+                        “As the mechanical lead it is my job to ensure that the
+                        team is continuously working on the car’s physical and
+                        CAD design in order to compete in any potential upcoming
+                        solar race. Securing the correct tools, guidance and
+                        services is essential.”
+                      </p>
+                      <p className="profile-description">
+                        “I would say that the first summer I joined the solar
+                        car team was the best. We would work late nights on a
+                        project that we loved and eat and bond together
+                        afterwards as a team.”
+                      </p>
+                    </section>
+                  </div>
+                  <div className="col-md-4 col-12-narrower lead">
+                    <section>
+                      <header>
+                        <div
+                          id="electrical-lead-img"
+                          className="profile-image"
+                        ></div>
+                        <h4>Bryan Thai</h4>
+                      </header>
+                      <p className="profile-header">
+                        Electrical Lead | Class of 2020 | Bioengineering
+                      </p>
+                      <p className="profile-description">
+                        “My role in the team is to provide a level of guidance
+                        in the design and implementation of the electrical
+                        system for the vehicle.”
+                      </p>
+                      <p className="profile-description">
+                        “My fondest memories are the bonfires that we usually
+                        host because they’re always fun and a great way to bond
+                        with the other members of the team.”
+                      </p>
+                    </section>
+                  </div>
                 </div>
-              </div>
-            </section>
-          </article>
-        </div>
+              </section>
+
+              <section className="wrapper style1 container special mt-3">
+                <div className="row">
+                  <div className="col-md-4 col-12-narrower other-members">
+                    <section>
+                      <div className="icon-wrapper">
+                        <span className="icon featured fa-briefcase symbol" />
+                      </div>
+                      <header>
+                        <h4>Operations Team</h4>
+                      </header>
+                      <p className="text-muted">
+                        Our job is to provide the tools necessary for the other
+                        subteams to progress effortlessly. This includes efforts
+                        towards public relations, sponsorship, and competition
+                        logistics.
+                      </p>
+                      <p className="text-muted">Camila Paik, Angela Yu</p>
+                    </section>
+                  </div>
+                  <div className="col-md-4 col-12-narrower other-members">
+                    <section>
+                      <div className="icon-wrapper">
+                        <span className="icon featured fa-cogs symbol" />
+                      </div>
+                      <header>
+                        <h4>Mechanical Team</h4>
+                      </header>
+                      <p className="text-muted">
+                        Responsibilities include the design, analysis, and
+                        manufacturing on the car’s structure
+                      </p>
+                      <p className="text-muted">
+                        Roy Choi, Janet He, Eugine Lin, Angela Yu, Canwin Tso,
+                        Sean Orellana, Jennie Coates, Arjun Ray, Arturo Medina,
+                        Jesse Sanchez, Dustin Tengdyantono, Billy Park, Jacob
+                        Watson, Sukhdeep Chera,
+                      </p>
+                    </section>
+                  </div>
+                  <div className="col-md-4 col-12-narrower other-members">
+                    <section>
+                      <div className="icon-wrapper">
+                        <span className="icon featured fa-bolt symbol" />
+                      </div>
+                      <header>
+                        <h4>Electrical Team</h4>
+                      </header>
+                      <p className="text-muted">
+                        Our job as the electrical subteam is to bring the car to
+                        life by providing the electricity needed to power the
+                        motor, whether that be through the solar array or the
+                        batteries. We also ensure that the vehicle is safe to
+                        operate using a suite of sensors throughout the car that
+                        monitor and provide feedback on its current condition.
+                      </p>
+                      <p className="text-muted">
+                        Bryan Thai, Dan Nguyen, Vivian Lee, Sara Dorr, Madeline
+                        Tjoa, Jonathan Venegas, Daniel Renfrow
+                      </p>
+                    </section>
+                  </div>
+                </div>
+              </section>
+            </article>
+          </div>
         </Container>
       </div>
     );
